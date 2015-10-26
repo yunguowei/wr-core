681a37aa301c4a90bfaa3b22c00665867d907284
Jason Wessel <jason.wessel@windriver.com>
2015-10-26 14:56:51 -0500

    Change all image names back to intel-corei7-64
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

--- install_templates/intel-x86/config-live.sh
+++ install_templates/intel-x86/config-live.sh
@@ -2,10 +2,10 @@ source config-usb.sh
 
 CONFIG_DIRS=`dirname $BASH_SOURCE`
 
-HDINSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-valleyisland-64.tar.bz2"
+HDINSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
 
-HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-valleyisland-64.tar.bz2 \
-                      ${ARTIFACTS_DIR}/cube-domE-valleyisland-64.tar.bz2 \
+HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2 \
+                      ${ARTIFACTS_DIR}/cube-domE-intel-corei7-64.tar.bz2 \
 "
-#HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-valleyisland-64.tar.bz2"
-INSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-valleyisland-64.tar.bz2"
+#HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2"
+INSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
