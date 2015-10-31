57c6a23228a019585ace01b9a6954fb296c93f34
fli <fupan.li@windriver.com>
2015-10-31 20:48:25 +0800

    wr-core: add distribution for x86 install template
    add distribution for x86 install template and fix the build commands
    bug of zedboard.
    
    Signed-off-by: fli <fupan.li@windriver.com>

--- install_templates/intel-x86/config-live.sh
+++ install_templates/intel-x86/config-live.sh
@@ -4,8 +4,8 @@ CONFIG_DIRS=`dirname $BASH_SOURCE`
 
 HDINSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
 
-HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2 \
-                      ${ARTIFACTS_DIR}/cube-domE-intel-corei7-64.tar.bz2 \
+HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2:vty=2 \
+                      ${ARTIFACTS_DIR}/cube-domE-intel-corei7-64.tar.bz2:monitored:console:vty=3 \
 "
 #HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2"
 INSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
