efe7ffed2466ec494ae40994acfb296ebac3f622
Weiwei Wang <weiwei.wang@windriver.com>
2015-11-26 09:47:10 +0800

    install_templates:set cube-server to be privileged
    set cube-server to be privileged for ZedBoards which means
    that the cube-server will support usb devices hot plug.
    
    Signed-off-by: fupan li <fupan.li@windriver.com>

--- install_templates/ZedBoard/config-usb.sh
+++ install_templates/ZedBoard/config-usb.sh
@@ -25,7 +25,7 @@ BOARD_NAME="xilinx-zynq"
 HDINSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-xilinx-zynq.tar.bz2"
 
 HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-xilinx-zynq.tar.bz2 \
-                      ${ARTIFACTS_DIR}/cube-server-xilinx-zynq.tar.bz2:monitored:console \
+                      ${ARTIFACTS_DIR}/cube-server-xilinx-zynq.tar.bz2:monitored:console:privileged \
 "
 INSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-xilinx-zynq.tar.bz2"
 
