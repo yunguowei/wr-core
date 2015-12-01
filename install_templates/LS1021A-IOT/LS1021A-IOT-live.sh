479d9e5ca600bffe1470d20e12fc2cf831fc7648
Guojian Zhou <guojian.zhou@windriver.com>
2015-12-01 15:19:38 +0800

    install_templates: Add the default partition layout for the LS1021A-IOT
    Set the "FDISK_PARTITION_LAYOUT" to use the special fdisk partition
    layout file for the LS1021A-IOT target. Because this target need deploy the
    u-boot into the SD card, and u-boot environment parameters are saved
    in the 1MB. Need to reserve beginning 2MB space for the u-boot and
    environment to avoid overriding the file system supperblock.
    
    Signed-off-by: Guojian Zhou <guojian.zhou@windriver.com>
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

--- install_templates/LS1021A-IOT/LS1021A-IOT-live.sh
+++ install_templates/LS1021A-IOT/LS1021A-IOT-live.sh
@@ -2,5 +2,6 @@ source config-usb.sh
 
 INSTALL_DTB="${ARTIFACTS_DIR}/uImage-ls1021a-iot.dtb"
 INSTALL_BOOTLOADER="$BASEDIR/../../layers/fsl-ls10xx/bootloader/ls1021aiot/u-boot-2015/u-boot-sd-ls1021aiot-2015.01-rcw_0x20.bin"
+FDISK_PARTITION_LAYOUT="$BASEDIR/../../overc-installer/sbin/fdisk-4-partition-SD-layout.txt"
 
 CONFIG_DIRS=`dirname $BASH_SOURCE`
