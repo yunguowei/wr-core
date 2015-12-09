7f3361a1b9000fa9be751489df84d594bf9ff643
Guojian Zhou <guojian.zhou@windriver.com>
2015-12-09 19:18:29 +0800

    install_templates: Add the default bootloader environment parameters file
    Add the default INSTALL_BOOTLOADER_ENV to make sure the LS1021A-IOT
    image could embed the suitable bootloader environment parameters.
    The new image could load the kernel and boot automatically by default.
    
    Signed-off-by: Guojian Zhou <guojian.zhou@windriver.com>
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

--- install_templates/LS1021A-IOT/LS1021A-IOT-live.sh
+++ install_templates/LS1021A-IOT/LS1021A-IOT-live.sh
@@ -3,5 +3,6 @@ source config-usb.sh
 INSTALL_DTB="${ARTIFACTS_DIR}/uImage-ls1021a-iot.dtb"
 INSTALL_BOOTLOADER="$BASEDIR/../../layers/fsl-ls10xx/bootloader/ls1021aiot/u-boot-2015/u-boot-sd-ls1021aiot-2015.01-rcw_0x20.bin"
 FDISK_PARTITION_LAYOUT="$BASEDIR/../../overc-installer/sbin/fdisk-4-partition-SD-layout.txt"
+INSTALL_BOOTLOADER_ENV="$BASEDIR/../../overc-installer/files/uboot-LS1021A-IOT.env.bin"
 
 CONFIG_DIRS=`dirname $BASH_SOURCE`
