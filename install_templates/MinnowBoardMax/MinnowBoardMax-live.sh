a2fcac037eefbd89d4e1e65feda2e2a2b0cee12c
fli <fupan.li@windriver.com>
2016-04-18 19:09:55 -0700

    add network prime configuration for intel-x86
    Signed-off-by: fli <fupan.li@windriver.com>

--- install_templates/MinnowBoardMax/MinnowBoardMax-live.sh
+++ install_templates/MinnowBoardMax/MinnowBoardMax-live.sh
@@ -5,7 +5,9 @@ CONFIG_DIRS=`dirname $BASH_SOURCE`
 HDINSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
 
 HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2:vty=2 \
-                      ${ARTIFACTS_DIR}/cube-domE-intel-corei7-64.tar.bz2:monitored:console:vty=3 \
+                      ${ARTIFACTS_DIR}/cube-domE-intel-corei7-64.tar.bz2:monitored:console:vty=3:net=1 \
 "
+NETWORK_DEVICE="enp2s0"
+
 #HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2"
 INSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
