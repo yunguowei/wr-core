6142595d855696189e7241da08299c538712d0f3
fli <fupan.li@windriver.com>
2016-05-18 00:45:34 -0700

    Change ComputeStick's network to wlp1s0
    This commit will make the ComputeStick-live.sh
    only workable for ComputeStick V2 board.
    
    Signed-off-by: fli <fupan.li@windriver.com>

--- install_templates/ComputeStick/ComputeStick-live.sh
+++ install_templates/ComputeStick/ComputeStick-live.sh
@@ -7,7 +7,8 @@ HDINSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
 HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2:vty=2 \
                       ${ARTIFACTS_DIR}/cube-domE-intel-corei7-64.tar.bz2:monitored:console:vty=3:net=1 \
 "
-NETWORK_DEVICE="wlan0"
+#NETWORK_DEVICE="wlan0"
+NETWORK_DEVICE="wlp1s0"
 
 #HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2"
 INSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
