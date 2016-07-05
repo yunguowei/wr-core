5ff7619d920e7c5f00c0cbef840e8b8a7a352227
fli <fupan.li@windriver.com>
2016-07-05 00:24:54 -0700

    wr-core: change LS1021A-IOT's installer function
    Since pulsar8's network modules has been changed,
    and it can passthough the network devices from
    essential to cube-gw directly, thus there is no
    need to share the same network namespace between
    essential and cube-gw container.
    
    Signed-off-by: fli <fupan.li@windriver.com>

--- install_templates/LS1021A-IOT/functions-local.sh
+++ install_templates/LS1021A-IOT/functions-local.sh
@@ -2,14 +2,10 @@
 my_local_post_func()
 {
     # cube-gw configuration
-    perl -p -i -e 's#^lxc.network.*##' ${TMPMNT}/var/lib/lxc/cube-gw/config
     echo "lxc.console = /dev/ttyS0" >> ${TMPMNT}/var/lib/lxc/cube-gw/config
-    echo "lxc.network.type = none" >> ${TMPMNT}/var/lib/lxc/cube-gw/config
     ln -sf /dev/null ${TMPMNT}/var/lib/lxc/cube-gw/rootfs/etc/systemd/system/getty.target.wants/serial-getty@ttyS0.service
     rm -f ${TMPMNT}/var/lib/lxc/cube-gw/rootfs/etc/systemd/system/multi-user.target.wants/named.service
     ln -sf /dev/null ${TMPMNT}/var/lib/lxc/cube-gw/rootfs/etc/systemd/system/named.service
-    rm -f ${TMPMNT}/var/lib/lxc/cube-gw/rootfs/etc/systemd/system/multi-user.target.wants/systemd-networkd.service
-    ln -sf /dev/null ${TMPMNT}/var/lib/lxc/cube-gw/rootfs/etc/systemd/system/systemd-networkd.service
     rm -f ${TMPMNT}/var/lib/lxc/cube-gw/rootfs/etc/systemd/system/systemd-resolved.service
     ln -sf /dev/null ${TMPMNT}/var/lib/lxc/cube-gw/rootfs/etc/systemd/system/systemd-resolved.service
     cat<<EOF>${TMPMNT}/var/lib/lxc/cube-gw/rootfs/etc/config/network
@@ -39,34 +35,27 @@ config device 'sim_card'
 	option present 'No'
 
 EOF
-    cat<<EOF>>${TMPMNT}/var/lib/lxc/cube-gw/rootfs/etc/config/dhcp
-
-config host
-	option name 'cube-dom0'
-	option mac '00:00:00:01:01:01'
-	option ip '192.168.10.111'
-EOF
     # essential configuration
     perl -p -i -e 's#cube-server#cube-gw#' ${TMPMNT}/etc/cube-cmd-server.conf
     rm -f ${TMPMNT}/etc/systemd/system/multi-user.target.wants/named.service
     ln -sf /dev/null ${TMPMNT}/etc/systemd/system/named.service
-    rm -f ${TMPMNT}/etc/systemd/system/multi-user.target.wants/systemd-networkd.service
-    ln -sf /dev/null ${TMPMNT}/etc/systemd/system/systemd-networkd.service
     # dom0 configuration
-    perl -p -i -e 's#^lxc.network.*##' ${TMPMNT}/var/lib/lxc/dom0/config
-    echo "lxc.network.type = veth" >> ${TMPMNT}/var/lib/lxc/dom0/config
-    echo "lxc.network.name = eth0" >> ${TMPMNT}/var/lib/lxc/dom0/config
-    echo "lxc.network.veth.pair = vethdom0" >> ${TMPMNT}/var/lib/lxc/dom0/config
-    echo "lxc.network.type = none" >> ${TMPMNT}/var/lib/lxc/cube-gw/config
     perl -p -i -e 's#cube-server#cube-gw#' ${TMPMNT}/var/lib/lxc/dom0/rootfs/etc/cube-cmd-server.conf
-    rm ${TMPMNT}/var/lib/lxc/dom0/rootfs/etc/systemd/network/*
-    cat<<EOF>${TMPMNT}/var/lib/lxc/dom0/rootfs/etc/systemd/network/wired.network
-[Match]
-Name=eth0
+    #config essential's dnsmasq service
+    rm -rf ${TMPMNT}/lib/systemd/system/dnsmasq.service
+    cat<<EOF>${TMPMNT}/lib/systemd/system/dnsmasq.service
+[Unit]
+Description=DNS forwarder and DHCP server
+After=network.target
 
-[Network]
-Address=192.168.10.111/24
-Gateway=192.168.10.1
+[Service]
+Type=forking
+PIDFile=/run/dnsmasq.pid
+ExecStart=/usr/bin/dnsmasq -x /run/dnsmasq.pid
+ExecReload=/bin/kill -HUP \$(/bin/cat /run/dnsmasq.pid)
+
+[Install]
+WantedBy=multi-user.target
 EOF
 }
 
