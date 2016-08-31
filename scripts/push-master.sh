fd8b6fa3fc4936178aac500ad5dc9cf19dfe698d
Jason Wessel <jason.wessel@windriver.com>
2016-08-31 15:20:54 -0500

    helper script for maintaining oe-core and bitbake
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

---
+++ scripts/push-master.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script is for the update of the oe-core and bitbake master
+# branches, which are nothing more than replicas from the upstream.
+
+(
+    cd layers/oe-core && \
+    git push git@github.com:WindRiver-OpenSourceLabs/oe-core master:master
+)
+
+(
+    cd layers/bitbake && \
+    git push git@github.com:WindRiver-OpenSourceLabs/bitbake master:master
+)
