5cee127228a665fe2965c01ed3256d37c8f5eb33
fli <fupan.li@windriver.com>
2016-09-22 19:10:22 -0700

    wr-core: redefine the NETWORK_DEVICE andNETWORK_DEVICE_CLASSES
    Signed-off-by: fli <fupan.li@windriver.com>

--- install_templates/Intel_NUC/IntelNUCServer-live.sh
+++ install_templates/Intel_NUC/IntelNUCServer-live.sh
@@ -7,7 +7,8 @@ HDINSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
 HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2:console:vty=2:mergepath=/usr,essential \
                       ${ARTIFACTS_DIR}/cube-server-intel-corei7-64.tar.bz2:monitored:privileged:console:vty=3,active:net=1:mergepath=/usr,essential,dom0 \
 		      "
-NETWORK_DEVICE="all"
+NETWORK_DEVICE_CLASSES="eth* wl* en*"
+NETWORK_DEVICE="eth0"
 
 INSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
 
