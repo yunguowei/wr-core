8211dd7a8189ee72894e31bef97e6e1af18543b5
fli <fupan.li@windriver.com>
2016-10-16 20:24:22 -0700

    wr-core: add the intel apollolake docs
    Signed-off-by: fli <fupan.li@windriver.com>

---
+++ install_templates/Intel_Apollolake/Intel-Apollolake-Desktop-live.sh
@@ -0,0 +1,14 @@
+source config-usb.sh
+
+CONFIG_DIRS=`dirname $BASH_SOURCE`
+
+HDINSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
+
+HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2:console:vty=2:mergepath=/usr,essential \
+                      ${ARTIFACTS_DIR}/cube-desktop-intel-corei7-64.tar.bz2:monitored:privileged:console:vty=3,active:net=1:mergepath=/usr,essential,dom0 \
+		      "
+NETWORK_DEVICE_CLASSES="eth* wl* en*"
+NETWORK_DEVICE="eth0"
+
+INSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
+
