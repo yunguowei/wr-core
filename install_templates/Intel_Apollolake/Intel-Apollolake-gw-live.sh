930131dc29044f69c9f10c978a7e5045605fda14
fli <fupan.li@windriver.com>
2016-11-23 19:21:55 -0800

    wr-core: fix the issue of cannot ping out from dom0
    Signed-off-by: fli <fupan.li@windriver.com>

--- install_templates/Intel_Apollolake/Intel-Apollolake-gw-live.sh
+++ install_templates/Intel_Apollolake/Intel-Apollolake-gw-live.sh
@@ -8,8 +8,9 @@ HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2:console
                       ${ARTIFACTS_DIR}/cube-gw-intel-corei7-64.tar.bz2:monitored:privileged:console:vty=3,active:net=1:mergepath=/usr,essential,dom0 \
 		      "
 NETWORK_DEVICE_CLASSES="eth* wl* en* usb*"
-NETWORK_DEVICE="eth0"
 
 INSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
+
+MY_LOCAL_FUNC_DIR=`readlink -f ${CONFIG_DIRS}`
 export LOCAL_POST_FUNCTION_DEFS="${MY_LOCAL_FUNC_DIR}/functions-local.sh"
 export LOCAL_CUSTOM_HDD_POST_FUNCS="my_local_post_func"
