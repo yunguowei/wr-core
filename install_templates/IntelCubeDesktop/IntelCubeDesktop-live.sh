ce63d961d690cc4cda494fa85a42ea7297be2603
Jason Wessel <jason.wessel@windriver.com>
2017-07-01 12:53:06 -0700

    Update for master-oci builds
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

--- install_templates/IntelCubeDesktop/IntelCubeDesktop-live.sh
+++ install_templates/IntelCubeDesktop/IntelCubeDesktop-live.sh
@@ -2,12 +2,15 @@ source config-usb.sh
 
 CONFIG_DIRS=`dirname $BASH_SOURCE`
 
+SCREEN_GETTY_CONSOLE=ttyS0,115200
+
 HDINSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
 
-HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2:console:vty=2:mergepath=/usr,essential \
-                      ${ARTIFACTS_DIR}/cube-desktop-intel-corei7-64.tar.bz2:monitored:console:vty=3:net=1:mergepath=/usr,essential,dom0 \
+HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2:console:vty=2:net=1 \
+                      ${ARTIFACTS_DIR}/cube-desktop-intel-corei7-64.tar.bz2:vty=3:net=3 \
 "
-NETWORK_DEVICE=""
+
+NETWORK_DEVICE="enp0s3"
 NETWORK_DEVICE_CLASSES="eth+ wl+ en+"
 
 #HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2"
