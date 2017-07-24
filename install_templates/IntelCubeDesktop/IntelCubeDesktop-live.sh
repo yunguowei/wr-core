dec3f2f51011fe293e7ea57138d74add41f5d407
Jason Wessel <jason.wessel@windriver.com>
2017-07-24 13:53:33 -0700

    Boots with console and new upreved OCI
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

--- install_templates/IntelCubeDesktop/IntelCubeDesktop-live.sh
+++ install_templates/IntelCubeDesktop/IntelCubeDesktop-live.sh
@@ -7,7 +7,7 @@ SCREEN_GETTY_CONSOLE=ttyS0,115200
 HDINSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
 
 HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2:console:vty=2:net=1 \
-                      ${ARTIFACTS_DIR}/cube-desktop-intel-corei7-64.tar.bz2:vty=3:net=3 \
+                      ${ARTIFACTS_DIR}/cube-desktop-intel-corei7-64.tar.bz2:console:vty=3:net=3 \
 "
 
 NETWORK_DEVICE="enp0s3"
