57c101f97f79de50650f90dc65ea32b957c4a534
Jason Wessel <jason.wessel@windriver.com>
2017-09-20 21:13:03 -0700

    IntelCubeDesktop-live.sh: restore cube-desktop to netprime
    Make the cube-desktop the netprime and also enable it to run the X
    server by setting the oci device setting to self.
    
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

--- install_templates/IntelCubeDesktop/IntelCubeDesktop-live.sh
+++ install_templates/IntelCubeDesktop/IntelCubeDesktop-live.sh
@@ -6,8 +6,8 @@ SCREEN_GETTY_CONSOLE=ttyS0,115200
 
 HDINSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
 
-HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2:console:vty=2:net=1 \
-                      ${ARTIFACTS_DIR}/cube-desktop-intel-corei7-64.tar.bz2:console:vty=3:net=3 \
+HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2:console:vty=2:net=3 \
+                      ${ARTIFACTS_DIR}/cube-desktop-intel-corei7-64.tar.bz2:console:vty=3:net=1:cube.device.mgr=self \
 "
 
 NETWORK_DEVICE="eth+ wl+ en+"
