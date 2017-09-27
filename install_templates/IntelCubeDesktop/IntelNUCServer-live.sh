f0aa8de2b53bfa01a0ce7265b044a0c43674749e
Fupan Li <fupan.li@windriver.com>
2017-09-26 22:15:47 -0700

    wr-core: enable cube-server as the foreground container
    Signed-off-by: Fupan Li <fupan.li@windriver.com>

--- install_templates/IntelCubeDesktop/IntelNUCServer-live.sh
+++ install_templates/IntelCubeDesktop/IntelNUCServer-live.sh
@@ -7,10 +7,23 @@ SCREEN_GETTY_CONSOLE=ttyS0,115200
 HDINSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
 
 HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2:console:vty=2:net=1 \
-                      ${ARTIFACTS_DIR}/cube-server-intel-corei7-64.tar.bz2:console:vty=3:net=3 \
+                      ${ARTIFACTS_DIR}/cube-server-intel-corei7-64.tar.bz2:console:vty=3,active:net=3 \
 "
 
 NETWORK_DEVICE="eth+ wl+ en+"
 
 #HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2"
 INSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
+
+export LOCAL_CUSTOM_HDD_POST_FUNCS="my_local_post_func"
+
+my_local_post_func()
+{
+        #copy the HAL id&passwd from dom0 to cube-srver/cube-gw/cube-desktop
+	CNS="cube-server cube-gw cube-desktop"
+        for c in ${CNS}; do
+                if [ -d ${TMPMNT}/opt/container/$c/rootfs ]; then
+                        cat ${TMPMNT}/opt/container/dom0/rootfs/var/lib/cube-cmd-server/auth.db > ${TMPMNT}/opt/container/$c/rootfs/etc/cube-cmd.auth
+                fi
+        done
+}
