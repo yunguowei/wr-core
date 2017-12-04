a27a337a15fccb4987a461341bfac7a5f73f8764
Fupan Li <fupan.li@windriver.com>
2017-12-03 18:13:49 -0800

    wr-core: add the post function to cubedesktop installer config file
    Signed-off-by: Fupan Li <fupan.li@windriver.com>

--- install_templates/IntelCubeDesktop/IntelCubeDesktop-live.sh
+++ install_templates/IntelCubeDesktop/IntelCubeDesktop-live.sh
@@ -14,3 +14,16 @@ NETWORK_DEVICE="eth+ wl+ en+"
 
 #HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2"
 INSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
+
+export LOCAL_CUSTOM_HDD_POST_FUNCS="my_local_post_func"
+
+my_local_post_func()
+{
+        #copy the HAL id&passwd from dom0 to cube-srver/cube-gw/cube-desktop
+        CNS="cube-server cube-gw cube-desktop"
+        for c in ${CNS}; do
+                if [ -d ${TMPMNT}/opt/container/$c/rootfs ]; then
+                        cat ${TMPMNT}/opt/container/dom0/rootfs/var/lib/cube-cmd-server/auth.db > ${TMPMNT}/opt/container/$c/rootfs/etc/cube-cmd.auth
+                fi
+        done
+}
