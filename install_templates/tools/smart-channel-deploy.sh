114c729c9179989c968da4c0209980876b158495
Fupan Li <fupan.li@windriver.com>
2017-12-11 21:48:50 -0800

    wr-core: upgrade the smart-channel-deploy.sh tool
    Signed-off-by: Fupan Li <fupan.li@windriver.com>

--- install_templates/tools/smart-channel-deploy.sh
+++ install_templates/tools/smart-channel-deploy.sh
@@ -60,49 +60,119 @@ if [ -z "$device" ]; then
 	exit 1
 fi
 
-if ! [ -e "${device}p3" ]; then
-	echo "[ERROR]: ${device}p3 cannot available!"
-	losetup -d ${device}
-	exit 1
-fi
-
-if ! [ -e "${device}p4" ]; then
-        echo "[ERROR]: ${device}p4 cannot available!"
-	losetup -d ${device}
+if ! [ -e "${device}p1" ]; then
+        echo "[ERROR]: ${device}p1 cannot available!"
+        losetup -d ${device}
         exit 1
 fi
 
-if ! [ -e "/z" ]; then
-	mkdir /z
-fi
-
-mount ${device}p3 /z
-if [ -e "/z/rootfs/var/lib/lxc" ]; then
-	mount ${device}p4 /z/rootfs/var/lib/lxc
-	cd /z/rootfs
-elif [ -e "/z/var/lib/lxc" ]; then
-	mount ${device}p4 /z/var/lib/lxc
-	cd /z
+if ! [ -e "${device}p2" ]; then
+	installer_sfr_image="Y"
+elif ! [ -e "${device}p3" ]; then
+	installer_image="Y"
 else
-	echo "[ERROR]: essential rootfs is not available"
-	umount /z
-        losetup -d ${device}
+	live_image="Y"
+	if ! [ -e "${device}p4" ]; then
+		echo "[ERROR]: ${device}p4 cannot available!"
+		losetup -d ${device}
+		exit 1
+	fi
 fi
 
-echo "[INFO] deploy $CONFIG_FILE to essential"
-cp -f $CONFIG_FILE var/lib/smart/config
+function deploy_live(){
+	device=$1
+	dir=$2	
 
-CN=`ls var/lib/lxc`
-
-for cn in $CN; do
-	if [ -d var/lib/lxc/$cn/rootfs/var/lib/smart ]; then
-		echo "[INFO] deploy $CONFIG_FILE to $cn"
-		cp -f $CONFIG_FILE var/lib/lxc/$cn/rootfs/var/lib/smart/config
+	mount ${device}p3 $dir
+	if [ -e "$dir/rootfs/var/lib/lxc" ]; then
+		mount ${device}p4 $dir/rootfs/var/lib/lxc
+		cd $dir/rootfs
+	elif [ -e "$dir/var/lib/lxc" ]; then
+		mount ${device}p4 $dir/var/lib/lxc
+		cd $dir
+	else
+		echo "[ERROR]: essential rootfs is not available"
+		umount $dir
+        	losetup -d ${device}
+		exit 1
 	fi
-done
 
-echo "[INFO] clean up the system"
-umount var/lib/lxc
-cd - >/dev/null 2>&1
-umount /z
+	if ! [ -d factory_tmp ]; then
+		mkdir factory_tmp
+	fi
+
+	mount -o subvolid=5 ${device}p3 factory_tmp
+	if [ -d factory_tmp/.factory/var/lib/lxc ]; then
+		mount -o subvolid=5 ${device}p4 factory_tmp/.factory/var/lib/lxc
+	fi
+
+	echo "[INFO] deploy $CONFIG_FILE to essential"
+	cp -f $CONFIG_FILE var/lib/smart/config
+
+	if [ -d factory_tmp/.factory/var/lib/smart ]; then
+		echo "[INFO] deploy $CONFIG_FILE to factory-reset backed essential"
+		cp -f $CONFIG_FILE factory_tmp/.factory/var/lib/smart/config
+	fi
+
+	CN=`ls var/lib/lxc`
+
+	for cn in $CN; do
+		if [ -d var/lib/lxc/$cn/rootfs/var/lib/smart ]; then
+			echo "[INFO] deploy $CONFIG_FILE to $cn"
+			cp -f $CONFIG_FILE var/lib/lxc/$cn/rootfs/var/lib/smart/config
+
+			if [ -d factory_tmp/.factory/var/lib/lxc/.factory/$cn/rootfs/var/lib/smart ]; then 
+				echo "[INFO] deploy $CONFIG_FILE to factory-reset backed $cn"
+				cp -f $CONFIG_FILE factory_tmp/.factory/var/lib/lxc/.factory/$cn/rootfs/var/lib/smart/config
+			fi
+		fi
+	done
+
+	echo "[INFO] clean up the system"
+	umount factory_tmp/.factory/var/lib/lxc
+	umount factory_tmp
+	rm -rf factory_tmp
+	umount var/lib/lxc
+	cd - >/dev/null 2>&1
+}
+
+function deploy_installer(){
+	device=$1
+	dir=$2
+
+	if [ "X$installer_sfr_image" == "XY" ]; then
+		mount ${device}p1 $dir
+	else
+		mount ${device}p2 $dir
+	fi
+
+	cd $dir
+	if [ -e "opt/installer/images" ]; then
+		echo "[INFO] deploy $CONFIG_FILE to `pwd`/opt/installer/images/config.smart"
+		cp -f $CONFIG_FILE opt/installer/images/config.smart
+	elif [ -e "./sfr.dsk" ]; then
+		echo "[INFO] deploy $CONFIG_FILE to `pwd`/sfr.dsk"
+		mount ./sfr.dsk EFI
+		cp -f $CONFIG_FILE EFI/installer/images/config.smart
+		umount EFI
+	fi
+	
+	cd - >/dev/null 2>&1
+}
+
+if ! [ -e "/z" ]; then
+        mkdir /z
+fi
+
+if [ "X$installer_sfr_image" == "XY" -o "X$installer_image" == "XY" ]; then
+	deploy_installer $device "/z"
+elif [ "X$live_image" == "XY" ]; then
+	deploy_live $device "/z"
+fi
+
+echo "umount directory /z"
+umount "/z"
+echo "release loop device $device"
 losetup -d ${device}
+
+
